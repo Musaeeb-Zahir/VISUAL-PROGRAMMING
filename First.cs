4a7a17bea2f74d666961a1ebcc6f503cc7d6d4ed
musaeeb-zahir <musaeebzahir@gmail.com>
2025-03-24 11:49:17 +0500

    this is first commit

---
+++ First.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace pro
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
